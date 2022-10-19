ca009d60c1ad1b438da286d446510d68fafb1006
victor.litoux <victor.litoux@epita.fr>
2022-10-19 15:52:08 +0200

    Refacto: create function for load and write image

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,30 @@
 #include <boost/gil/extension/io/png.hpp>
 
 #define USE_GPU false
-using namespace boost::gil;
+namespace gil = boost::gil;
+
+//using namespace boost::gil;
+void writeImage(const std::string &path, gil::rgb8_image_t &image)
+{
+	write_view(path, gil::view(image), gil::png_tag());
+}
+
+gil::rgb8_image_t loadImage(const std::string &path)
+{
+	gil::rgb8_image_t image;
+	read_and_convert_image(path, image, gil::png_tag());
+	return image;
+
+}
+
 int main()
 {
+	gil::rgb8_image_t image = loadImage("img/img_1.png");
+	writeImage("img/test.png", image);
 #if (USE_GPU)
 	use_gpu();
 #else
 	use_cpu();
 #endif
-	rgb8_image_t input;
-	std::string image_path = "img/img.png";
-
-
-	read_and_convert_image(image_path,input, png_tag());
-	/*
-	std::cout << input.height() << "\n";*/
-	write_view("img/test.png", view(input),png_tag());
 	return 0;
 }
