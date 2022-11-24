3a647877de5250e356ebee92f401587872fc818e
victor.litoux <victor.litoux@epita.fr>
2022-11-24 15:43:47 +0100

    Refacto: split matrix_image from render_cpu

--- src/render_cpu.hpp
+++ src/render_cpu.hpp
@@ -8,53 +8,12 @@
 #include <boost/gil/extension/io/png.hpp>
 #include <vector_types.h>
 #include <spdlog/spdlog.h>
+#include "matrix_image/matrix_image.h"
 
-namespace gil = boost::gil;
 
-
-void _abortError(const char *msg, const char *filename, const char *fname, int line);
-
-#define abortError(msg) _abortError(msg,__FILE__, __FUNCTION__, __LINE__)
-
-struct kernel
-{
-	float **buf;
-	size_t size;
-};
-
-template<typename T>
-struct matrixImage
-{
-	T *buffer;
-	size_t width;
-	size_t height;
-
-	matrixImage(size_t width, size_t height) : width(width), height(height)
-	{
-		buffer = new T[width * height];
-	}
-
-	T *at(size_t w, size_t h) const
-	{
-		if (w > width || h > height)
-		{
-			abortError("Access out of bound");
-		}
-		return &buffer[h * width + w];
-	}
-
-	void set(size_t w, size_t h, T &value)
-	{
-		*this->at(w, h) = value;
-	}
-};
-
-template<typename T>
-matrixImage<T> *toMatrixImage(gil::rgb8_image_t &image);
 
 void useCpu(gil::rgb8_image_t &image);
 
-matrixImage<uchar3> * toMatrixImage(gil::rgb8_image_t &image);
 
 void toGrayscale(matrixImage<uchar4> *buf_in, matrixImage<float> *buf_out);
 void gaussianBlur(matrixImage<float> *buf_in, matrixImage<float> *buf_out);
